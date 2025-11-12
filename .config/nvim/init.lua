ee7bbc7c1740b9ffa38ecf44519f6e5747900a98
onraj55555 <jarnovanruymbeke@gmail.com>
2025-11-12 20:51:06 +0100

    sync

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -681,6 +681,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         markdown_oxide = {},
+        ols = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -704,6 +705,7 @@ require('lazy').setup({
             },
           },
         },
+        zls = {},
       }
 
       -- Ensure the servers and tools above are installed
